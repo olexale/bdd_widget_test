c0631e03d5c5ae07cd4c363977c8fe6554fc02eb
Oleksandr Leushchenko <olexa.le@gmail.com>
2022-05-24 18:56:03 +0300

    Add Diacritics tests

---
+++ test/diacritics_test.dart
@@ -0,0 +1,37 @@
+import 'package:bdd_widget_test/src/feature_file.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Diacritics ', () {
+    const featureFile = '''
+Feature: Testing feature
+  Scenario: Testing scenario
+    Given los diacríticos son útil
+''';
+
+    const expectedFeatureDart = '''
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_import, directives_ordering
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import './step/los_diacriticos_son_util.dart';
+
+void main() {
+  group(\'\'\'Testing feature\'\'\', () {
+    testWidgets(\'\'\'Testing scenario\'\'\', (tester) async {
+      await losDiacriticosSonUtil(tester);
+    });
+  });
+}
+''';
+
+    final feature = FeatureFile(
+      featureDir: 'test.feature',
+      package: 'test',
+      input: featureFile,
+    );
+    expect(feature.dartContent, expectedFeatureDart);
+  });
+}
